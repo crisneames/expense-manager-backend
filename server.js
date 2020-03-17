c818dd118f1153a157d3d607f03b8bb8f84b40e5
Cristi Neames <crisneames.gmail.com>
2020-03-17 09:50:30 -0500

    finish server.js

--- server.js
+++ server.js
@@ -1,11 +1,35 @@
 const express = require('express')
 const app = express()
 const PORT = 3003
+const expenseController = require('./controllers/expense.js')
+const mongoose = require('mongoose')
+const cors = require('cors')
+// Middleware
+app.use(express.json())
 
-app.get('/', (req, res) => {
-  res.send('Hello World');
-});
+const whitelist = ['http://localhost:3000', 'https://fathomless-sierra-68956.herokuapp.com']
 
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+app.use(cors(corsOptions)) // all routes are now exposed, sometimes you just want to limit access (ie OMDB - it's ok for anyone to see the movies, but you don't want just anyone updating the movies)
+
+// Error / Disconnection
+mongoose.connection.on('error', err => console.log(err.message + ' is Mongod not running?'))
+mongoose.connection.on('disconnected', () => console.log('mongo disconnected'))
+
+mongoose.connect('mongodb://localhost:27017/expenseManager', { useNewUrlParser: true, UnifiedTopology: true })
+mongoose.connection.once('open', ()=>{
+    console.log('connected to mongoose...')
+})
+
+app.use('/expense', expenseController)
 
 app.listen(PORT, () => {
   console.log('🎉🎊', 'celebrations happening on port', PORT, '🎉🎊',)
