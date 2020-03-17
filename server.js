aa7bf0c9421de7b1356347e6020d712660a374a3
Cristi Neames <crisneames.gmail.com>
2020-03-17 14:25:44 -0500

    heroku connect issues

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express()
 const PORT = 3003
 const expenseController = require('./controllers/expense.js')
 const mongoose = require('mongoose')
-// const cors = require('cors')
+const cors = require('cors')
 
 // Middleware
 app.use(express.json())
@@ -20,26 +20,26 @@ const whitelist = ['http://localhost:3000', 'https://fathomless-sierra-68956.her
 //   }
 // }
 
-// const corsOptions = {
-//     origin: function (origin, callback){
-//         if(whitelist){
-//             callback(null, true)
-//         }else{
-//             callback(new Error('Not allowed by CORS'))
-//         }
-//     }
-// }
+const corsOptions = {
+    origin: function (origin, callback){
+        if(whitelist){
+            callback(null, true)
+        }else{
+            callback(new Error('Not allowed by CORS'))
+        }
+    }
+}
 
-// app.use(cors(corsOptions)) // all routes are now exposed, sometimes you just want to limit access (ie OMDB - it's ok for anyone to see the movies, but you don't want just anyone updating the movies)
+app.use(cors(corsOptions)) // all routes are now exposed, sometimes you just want to limit access (ie OMDB - it's ok for anyone to see the movies, but you don't want just anyone updating the movies)
 
 // Error / Disconnection
-mongoose.connection.on('error', err => console.log(err.message + ' is Mongod not running?'))
-mongoose.connection.on('disconnected', () => console.log('mongo disconnected'))
-
-mongoose.connect('mongodb://localhost:27017/expenseManager', { useNewUrlParser: true, useUnifiedTopology: true })
-mongoose.connection.once('open', ()=>{
-    console.log('connected to mongoose...')
-})
+// mongoose.connection.on('error', err => console.log(err.message + ' is Mongod not running?'))
+// mongoose.connection.on('disconnected', () => console.log('mongo disconnected'))
+//
+// mongoose.connect('mongodb://localhost:27017/expenseManager', { useNewUrlParser: true, useUnifiedTopology: true })
+// mongoose.connection.once('open', ()=>{
+//     console.log('connected to mongoose...')
+// })
 
 
 app.use('/expense', expenseController)
