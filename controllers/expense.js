c35c4c58e236be7d04f93003e9938166c03cbbcb
Cristi Neames <crisneames.gmail.com>
2020-03-17 12:16:01 -0500

    getting heroku to work with app

--- controllers/expense.js
+++ controllers/expense.js
@@ -2,15 +2,6 @@ const expense = require('express').Router()
 const Expense = require('../models/expense.js')
 
 // Index route  - show expenses
-expense.get('/', (req, res) => {
-  res.send('Hello World');
-//   Expense.find({}, (err, foundExpense) => {
-// res.send('Hello World')
-//     if (err) {
-//       res.status(400).json({ error: err.message })
-//     }
-//     res.status(200).json(foundExpense)
-//   })
-});
+
 
 module.exports = expense
