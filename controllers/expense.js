f487fd70494e3f33fbcf6aea25a6842c0a1dd40f
Cristi Neames <crisneames.gmail.com>
2020-03-18 21:18:59 -0500

    backend route fix

--- controllers/expense.js
+++ controllers/expense.js
@@ -1,16 +1,44 @@
 const expense = require('express').Router()
-const Expense = require('../modules/expense.js')
+//const Expense = require('../models/expense.js')
 
-// Index route  - show expenses
+// Index route
 expense.get('/', (req, res) => {
-  res.send('Hello World');
-//   Expense.find({}, (err, foundExpense) => {
-// res.send('Hello World')
-//     if (err) {
-//       res.status(400).json({ error: err.message })
-//     }
-//     res.status(200).json(foundExpense)
-//   })
-});
+  Expense.find({}, (err, foundExpense) => {
+    if (err) {
+      res.status(400).json({ error: err.message })
+    }
+    res.status(200).json(foundExpense)
+  })
+})
+
+// Create route
+expense.post('/', async(req, res)  => {
+  Expense.create(req.body, (error, createdExpense) => {
+    if(error) {
+      res.status(400).json({error: error.message})
+
+    }
+    res.status(200).send(createdExpense)
+  })
+})
+
+// Delete route
+expense.delete('/:id', (req, res) => {
+  Expense.findByIdAndRemove(req.params.id, (err, deletedExpense) => {
+    if (err) {
+      res.status(400).json({ error: err.message })
+    }
+    res.status(200).json(deletedExpense)
+  })
+})
+// Update route
+expense.put('/:id', (req, res) => {
+  Expense.findByIdAndUpdate(req.params.id, req.body, { new: true }, (err, updatedExpense) => {
+    if (err) {
+      res.status(400).json({ error: err.message })
+    }
+    res.status(200).json(updatedExpense)
+  })
+})
 
 module.exports = expense
